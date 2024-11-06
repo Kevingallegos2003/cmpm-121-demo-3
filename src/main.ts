884cbfe9095b4aa302f56c34b1717131f04bd701
Kevin Gallegos <kevingallegos2003@gmail.com>
2024-11-06 14:47:21 -0800

    Button

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 // todo
 const app = document.querySelector<HTMLDivElement>("#app")!;
 const black = document.createElement("button");
-black.innerHTML = "Click ME";
+black.innerHTML = "Click ME"; //button
 app.append(black);
 black.addEventListener("click", () => {
   alert("You rn: 🤓");
